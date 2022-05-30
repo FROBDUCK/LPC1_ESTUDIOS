0ec7f224bb7bf8ce83c61babc11af937e13e364a
victor luiz almeida medeiros <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-05-30 02:02:22 -0300

    Add files via upload

---
+++ posineg.py
@@ -0,0 +1,7 @@
+num = float(input('digite um numero: '))
+if num > 0:
+    print('este numero é positivo')
+elif num == 0:
+    print('este numero é neutro')
+else:
+    print('este numero é negativo')
